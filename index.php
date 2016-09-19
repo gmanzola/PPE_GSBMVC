c6b2ebb0d29b5033b953b2214d82d26b0a7931b1
JEAN <JEAN@ASUS>
2016-09-19 16:14:36 +0200

    test

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 require_once("include/fct.inc.php");
 require_once ("include/class.pdogsb.inc.php");
 include("vues/v_entete.php") ;
+echo "ok";
 session_start();
 $pdo = PdoGsb::getPdoGsb();
 $estConnecte = estConnecte();
